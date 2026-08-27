e7894c773c62d79c0bb0e69644fcd3800e3f6069
yugami <yugamissu@gmail.com>
2026-08-27 14:00:17 +0800

    content: rename site metadata to Rush Leo

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,5 +4,5 @@ export async function GET({ site }) {
   const articles = (await getCollection("articles", ({ data }) => !data.draft))
     .sort((a, b) => b.data.publishedAt.valueOf() - a.data.publishedAt.valueOf());
   const items = articles.map((article) => `<item><title><![CDATA[${article.data.title}]]></title><link>${new URL(`/writing/${article.id}`, site)}</link><description><![CDATA[${article.data.description}]]></description><pubDate>${article.data.publishedAt.toUTCString()}</pubDate></item>`).join("");
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel><title>不息的文章</title><link>${site}</link><description>关于设计工程、个人档案与日常观察。</description>${items}</channel></rss>`, { headers: { "Content-Type": "application/xml" } });
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel><title>Rush Leo 的文章</title><link>${site}</link><description>关于设计工程、个人档案与日常观察。</description>${items}</channel></rss>`, { headers: { "Content-Type": "application/xml" } });
 }
